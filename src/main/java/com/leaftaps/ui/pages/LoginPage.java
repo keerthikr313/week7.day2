238d5b0023b03e9065c93411fe54016c9b361651
keerthikr313 <104002660+keerthikr313@users.noreply.github.com>
2022-06-18 16:14:42 +0530

    Update LoginPage.java

--- src/main/java/com/leaftaps/ui/pages/LoginPage.java
+++ src/main/java/com/leaftaps/ui/pages/LoginPage.java
@@ -1,29 +1,41 @@
 package com.leaftaps.ui.pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import com.leaftaps.ui.base.ProjectSpecificMethod;
 
 public class LoginPage extends ProjectSpecificMethod {
-
+	
+	@FindBy(how = How.ID, using="username") private WebElement elementUsername;
+	@FindBy(how=How.ID, using="password") private WebElement elementPassword;
+	@FindBy(how=How.CLASS_NAME, using="decorativeSubmit") private WebElement elementLogin;
+	
+	
 	public LoginPage(RemoteWebDriver inWardDriver) {
 		this.driver = inWardDriver;
+		PageFactory.initElements(driver, this);
+
 	}
 	
 	public LoginPage typeUsername(String username) {
-		driver.findElement(By.id(property.getProperty("loginPage.username.id"))).sendKeys(username);
+		//driver.findElement(By.id(property.getProperty("loginPage.username.id"))).sendKeys(username);
+		elementUsername.sendKeys(username);
 		return this;
 	}
 	
 	public LoginPage typePassword(String password) {
-		driver.findElement(By.id(property.getProperty("loginPage.password.id"))).sendKeys(password);
+		//driver.findElement(By.id(property.getProperty("loginPage.password.id"))).sendKeys(password);
+		elementPassword.sendKeys(password);
 		return this;
 	}
 	
 	public WelcomePage ClickLoginButton() {
-		driver.findElement(By.className(property.getProperty("loginPage.Login.class"))).click();
+		//driver.findElement(By.className(property.getProperty("loginPage.Login.class"))).click();
+		elementLogin.click();
 		return new WelcomePage(driver);
 
 	}
