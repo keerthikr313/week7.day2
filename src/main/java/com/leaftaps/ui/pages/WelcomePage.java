75000b6af8c63a457016eeb33f302fcdc5be5539
keerthikr313 <104002660+keerthikr313@users.noreply.github.com>
2022-06-18 16:16:35 +0530

    Update WelcomePage.java

--- src/main/java/com/leaftaps/ui/pages/WelcomePage.java
+++ src/main/java/com/leaftaps/ui/pages/WelcomePage.java
@@ -1,17 +1,25 @@
 package com.leaftaps.ui.pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import com.leaftaps.ui.base.ProjectSpecificMethod;
 
 public class WelcomePage extends ProjectSpecificMethod {
+	
+	@FindBy(how=How.LINK_TEXT, using="CRM/SFA") private WebElement elementCRMSFA;
+ 
 	public WelcomePage(RemoteWebDriver inWardDriver) {
 		this.driver = inWardDriver;
-		
+		PageFactory.initElements(driver, this);
+
 	}
 	public HomePage clickCRMSFA() {
-		driver.findElement(By.linkText(property.getProperty("welcomePage.crmsfa.linktext"))).click();
+		//driver.findElement(By.linkText(property.getProperty("welcomePage.crmsfa.linktext"))).click();
+		elementCRMSFA.click();
 		return new HomePage(driver);
 	}
 
