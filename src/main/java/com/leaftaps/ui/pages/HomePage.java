95bdc6c30c0bef4a989e94d26e3762f446a7601d
keerthikr313 <104002660+keerthikr313@users.noreply.github.com>
2022-06-18 16:13:51 +0530

    Update HomePage.java

--- src/main/java/com/leaftaps/ui/pages/HomePage.java
+++ src/main/java/com/leaftaps/ui/pages/HomePage.java
@@ -1,18 +1,26 @@
 package com.leaftaps.ui.pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import com.leaftaps.ui.base.ProjectSpecificMethod;
 
 public class HomePage extends ProjectSpecificMethod {
+	
+	@FindBy(how=How.LINK_TEXT, using="Leads") private WebElement elementLeadButton;
+
 	public HomePage(RemoteWebDriver inWardDriver) {
 		this.driver = inWardDriver;
-		
+		PageFactory.initElements(driver, this);
+
 	}
 	
 	public MyLeadsPage clickLeads() {
-		driver.findElement(By.linkText(property.getProperty("homePage.leads.linktext"))).click();
+		//driver.findElement(By.linkText(property.getProperty("homePage.leads.linktext"))).click();
+		elementLeadButton.click();
 		return new MyLeadsPage(driver);
 	}
 
