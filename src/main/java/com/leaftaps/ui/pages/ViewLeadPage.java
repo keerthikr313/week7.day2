d2828b87e11a83b6e0d712273e3bd007f372b396
keerthikr313 <104002660+keerthikr313@users.noreply.github.com>
2022-06-18 16:16:03 +0530

    Update ViewLeadPage.java

--- src/main/java/com/leaftaps/ui/pages/ViewLeadPage.java
+++ src/main/java/com/leaftaps/ui/pages/ViewLeadPage.java
@@ -1,17 +1,27 @@
 package com.leaftaps.ui.pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 import com.leaftaps.ui.base.ProjectSpecificMethod;
 
 public class ViewLeadPage extends ProjectSpecificMethod {
+	
+	@FindBy(how=How.ID, using="viewLead_companyName_sp") private WebElement elementViewLeadID;
+
 	public ViewLeadPage(RemoteWebDriver inWardDriver) {
 		this.driver = inWardDriver;
+		PageFactory.initElements(driver, this);
+
 	}
 	
 	public ViewLeadPage verifyLeadId() {
-		String leadID = driver.findElement(By.id("viewLead_companyName_sp")).getText().replaceAll("[^0-9]", " ");
-		System.out.println(leadID);
+		//String leadID = driver.findElement(By.id("viewLead_companyName_sp")).getText().replaceAll("[^0-9]", " ");
+		String LeadID = elementViewLeadID.getText().replaceAll("[^0-9]", "");
+		System.out.println(LeadID);
 		return this;
 	}
 
