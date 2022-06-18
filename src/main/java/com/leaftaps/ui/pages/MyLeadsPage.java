4d3c7f6d6991f8d091ee6bb24025fe38b4e1b23e
keerthikr313 <104002660+keerthikr313@users.noreply.github.com>
2022-06-18 16:15:21 +0530

    Update MyLeadsPage.java

--- src/main/java/com/leaftaps/ui/pages/MyLeadsPage.java
+++ src/main/java/com/leaftaps/ui/pages/MyLeadsPage.java
@@ -1,18 +1,26 @@
 package com.leaftaps.ui.pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import com.leaftaps.ui.base.ProjectSpecificMethod;
 
 public class MyLeadsPage extends ProjectSpecificMethod {
+	
+	@FindBy(how=How.LINK_TEXT, using="Create Lead") private WebElement elementCreateLeadButton;
+
 	public MyLeadsPage(RemoteWebDriver inWardDriver) {
 		this.driver = inWardDriver;
-		
+		PageFactory.initElements(driver, this);
+
 	}
 	
 	public CreateLeadPage clickCreateLead() {
-		driver.findElement(By.linkText(property.getProperty("leadsPage.createLead.linktext"))).click();
+		//driver.findElement(By.linkText(property.getProperty("leadsPage.createLead.linktext"))).click();
+		elementCreateLeadButton.click();
 		return new CreateLeadPage(driver);
 	}
 
